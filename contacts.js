f8b97a6aa0f11f7cda5ea91a9c1f4b0d9d2d54b6
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2023-03-23 14:17:44 +0200

    update index

--- contacts.js
+++ contacts.js
@@ -17,7 +17,7 @@ const getContactById = async (contactId) => {
 
 const removeContact = async (contactId) => {
   const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id !== contactId);
+  const index = contacts.findIndex((item) => item.id === contactId);
   if (index === -1) {
     return null;
   }
